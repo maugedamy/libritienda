b54726660578ac473b1cbb0aa62f444cc741ebc7
mauged <m.eugenia.damy@gmail.com>
2022-08-16 20:15:17 -0300

    navBar link

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,68 +1,69 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import CartWidget from '../CartWidget';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import CartWidget from "../CartWidget";
 
 function NavBar() {
-    return (
-        <>
-            {/* {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => ( */}
-            {['sm'].map((expand) => (
-                <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-                    <Container fluid>
-                        <Navbar.Brand href="/">LibriTienda</Navbar.Brand>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas
-                            id={`offcanvasNavbar-expand-${expand}`}
-                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                            placement="end"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                    Offcanvas
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end flex-grow-1 pe-3">
-                                    <Nav.Link href="/">Inicio</Nav.Link>
-                                    <Nav.Link href="/category/colecciones">Colecciones</Nav.Link>
-                                    {/* <Nav.Link href="/category/generos">Géneros</Nav.Link> */}
-                                    <NavDropdown
-                                        title="Idiomas"
-                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
-                                    >
-                                        <NavDropdown.Item href="/category/espanol">Español</NavDropdown.Item>
-                                        <NavDropdown.Item href="/category/ingles">
-                                            Inglés
-                                        </NavDropdown.Item>
-                                        {/* <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action5">
-                                            Géneros
-                                        </NavDropdown.Item> */}
-                                    </NavDropdown>
-                                    <Nav.Link href="./carrito"><CartWidget totalQuantity={5}/></Nav.Link>
-                                </Nav>
-                                <Form className="d-flex">
-                                    <Form.Control
-                                        type="search"
-                                        placeholder="Buscar"
-                                        className="me-2"
-                                        aria-label="Search"
-                                    />
-                                    <Button variant="outline-success">Buscar</Button>
-                                </Form>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {["sm"].map((expand) => (
+        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="/">LibriTienda</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Link className="nav-link" to="/">Inicio</Link>
+                  <Link className="nav-link" to="/category/colecciones">Colecciones</Link>
+                  {/* <Link to="/category/generos">Géneros</Link> */}
+                  <NavDropdown
+                    title="Idiomas"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item>
+                      <Link className="nav-link" to="/category/espanol">Español</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>
+                      <Link className="nav-link" to="/category/ingles">Inglés</Link>
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                  </NavDropdown>
+                  <Link className="nav-link" to="./carrito">
+                    <CartWidget />
+                  </Link>
+                </Nav>
+                {/* <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Buscar"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Buscar</Button>
+                </Form> */}
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
 }
 
 export default NavBar;
