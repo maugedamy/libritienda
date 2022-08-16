9519a6d66fbfa6e6be381c3b400ef57e463b9088
mauged <m.eugenia.damy@gmail.com>
2022-08-16 13:33:52 -0300

    primera entrega corrección navegación

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,10 +1,8 @@
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
 
-function Item({ id, title, description, price, pictureUrl}) {
-    const loadLibrosDetails = () => {
-        window.location.href=`/detalles/${id}`
-    }
+function Item({ id, title, price, pictureUrl}) {
 
     return (
         <Card style={{ width: '25rem' }}>
@@ -16,8 +14,12 @@ function Item({ id, title, description, price, pictureUrl}) {
             <Card.Title style={{textAlign:'center'}}>{`$${price}`}</Card.Title>
             {/* <Card.Text>{`${description}`}</Card.Text> */}
             {/* <ItemCounter /> */}
+            <Link to={`/detalles/${id}`}>
+                <button>Ver detalles</button>
+            </Link>
             </Card.Body>
-            {<button onClick={loadLibrosDetails}>Ver detalles</button>}
+            {/* {<button onClick={loadLibrosDetails}>Ver detalles</button>} */}
+            
         {/* </div>          */}
         </Card>
     );
